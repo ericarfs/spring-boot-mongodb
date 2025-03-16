9274304da0a03695307992318337d57cac1b020a
Erica Ribeiro <ericaribeiro@usp.br>
2025-03-16 20:37:06 -0300

    Adding comments to posts

--- src/main/java/com/ericarfs/spring_boot_mongodb/config/Instantiation.java
+++ src/main/java/com/ericarfs/spring_boot_mongodb/config/Instantiation.java
@@ -9,6 +9,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
 import com.ericarfs.spring_boot_mongodb.dto.AuthorDTO;
+import com.ericarfs.spring_boot_mongodb.dto.CommentDTO;
 import com.ericarfs.spring_boot_mongodb.entity.Post;
 import com.ericarfs.spring_boot_mongodb.entity.User;
 import com.ericarfs.spring_boot_mongodb.repository.PostRepository;
@@ -41,6 +42,13 @@ public class Instantiation implements CommandLineRunner{
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraço!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 		
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite!", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+		
+		post1.getComments().add(c1);
+		post1.getComments().add(c2);
+		post2.getComments().add(c3);
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
