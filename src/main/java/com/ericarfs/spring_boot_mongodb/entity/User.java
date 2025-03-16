b394269d64fe83a54e4a96c7eecc932a9836b5f1
Erica Ribeiro <ericaribeiro@usp.br>
2025-03-16 19:39:57 -0300

    Create Post entity

--- src/main/java/com/ericarfs/spring_boot_mongodb/entity/User.java
+++ src/main/java/com/ericarfs/spring_boot_mongodb/entity/User.java
@@ -3,18 +3,19 @@ package com.ericarfs.spring_boot_mongodb.entity;
 import java.io.Serializable;
 import java.util.Objects;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection="tb_user")
 public class User implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@Id
 	private String id;
 	private String name;
 	private String email;
 	
 	public User() {
-		// TODO Auto-generated constructor stub
 	}
 
 	public User(String id, String name, String email) {
