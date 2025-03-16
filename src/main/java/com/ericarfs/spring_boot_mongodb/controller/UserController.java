03596296e4f8648c5ce6564011fb4bca32d28732
Erica Ribeiro <ericaribeiro@usp.br>
2025-03-16 19:07:50 -0300

    Adding POST, PUT, DELETE User methods

--- src/main/java/com/ericarfs/spring_boot_mongodb/controller/UserController.java
+++ src/main/java/com/ericarfs/spring_boot_mongodb/controller/UserController.java
@@ -1,14 +1,17 @@
 package com.ericarfs.spring_boot_mongodb.controller;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,5 +35,34 @@ public class UserController {
 								.collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
 	}
-
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable String id){
+		User user = service.findById(id);
+		UserDTO userDto = new UserDTO(user);
+		
+		return ResponseEntity.ok().body(userDto);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto){
+		User obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		return ResponseEntity.status(HttpStatus.CREATED).build();
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable String id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Void> update(@PathVariable String id, @RequestBody UserDTO objDto){
+		User obj = service.fromDTO(objDto);
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+	}
+	
 }
--- src/main/java/com/ericarfs/spring_boot_mongodb/service/UserService.java
+++ src/main/java/com/ericarfs/spring_boot_mongodb/service/UserService.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.ericarfs.spring_boot_mongodb.dto.UserDTO;
 import com.ericarfs.spring_boot_mongodb.entity.User;
+import com.ericarfs.spring_boot_mongodb.exceptions.ResourceNotFoundException;
 import com.ericarfs.spring_boot_mongodb.repository.UserRepository;
 
 @Service
@@ -16,6 +18,38 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repository.findAll();
+	}
+	
+	public User findById(String id) {
+		return repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("User with id "+id+" not found."));
+	}
+	
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
+	
+	public void delete(String id) {
+		if (repository.existsById(id))
+			repository.deleteById(id);
+		else
+			throw new ResourceNotFoundException("User with id "+id+" not found.");
+			
+	}
+	
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repository.save(newObj);			
+	}
+
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
 	}
 
 }
